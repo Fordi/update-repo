a967a928a88f1c9cbde79ed6b1d79476d4fc20cf
Bryan Elliott <fordiman@gmail.com>
2025-05-08 16:04:13 -0400

    Added CLI args

--- README.md
+++ README.md
@@ -33,3 +33,27 @@ update-repo all
 
 Target repository must have a `bin` folder containing executables to be installed to `/usr/local/bin`.
 
+Full usage:
+
+`update-repo <subcommand> [...options]`
+
+## Subcommands
+
+ - `ls` | `list` - list installed repositories
+ - `u` | `uninstall` `<repo>` - uninstall repository
+ - `i` | `install` `<repo>` - install a repository
+     - `-b` | `-branch` `<branch>` - specify a preferred branch (default: main/master)
+ - `c` | `configure` - modify global configuration
+ - `d` | `dump-config` - dump the current global configuration
+ - `U` | `update` `<repo>` - update repository
+      - `-b` | `-branch` `<branch>` - change preferred branch
+ - `UA` | `update all` - update all installed repositories
+
+## Common flags:
+ - `-s` | `-sources` - specify sources folder (default /opt or ~/.repos)
+ - `-t` | `-target` - specify target folder (default /usr/local/bin or first item in PATH under ~)
+
+## `<repo>`
+ - project name - a GitHub repo assumed to be owned by $USER, e.g., `update-repo install foo` for user bar would be treated as `git@github.com:bar/foo.git`
+ - owner/project - assumed to be a GitHub repo, e.g., `foo/bar` would become https://github.com/foo/bar.git
+ - a url - a full URL to a git repo; SSH and HTTPS supported
--- update-repo.sh
+++ update-repo.sh
@@ -1,56 +1,355 @@
 #!/bin/bash
-REPO="$1"; shift
-TARGET="${1:-/usr/local/bin}"; shift
+SELF="$(basename "$0")"
 
-SELF="/usr/local/bin/update-repo"
 
-if [[ $UID != 0 ]]; then
-  echo "Must be run as root" >&2
-  exit -1
-fi
+LOCAL_SOURCES=
+LOCAL_TARGET=
+LOCAL_CONF=
 
-if [[ -z "$REPO" || "$REPO" == "all" ]]; then
+function conf-file() {
+  if [[ -n "$LOCAL_CONF" ]]; then
+    echo "$LOCAL_CONF"
+  elif [[ $UID == 0 ]]; then
+    echo "/etc/update-repo/global.conf"
+  else
+    echo "$HOME/.config/update-repo/global.conf"
+  fi
+}
+
+function conf-path() {
+  dirname "$(conf-file)"
+}
+
+function conf-name() {
+  basename "$(conf-file)"
+}
+
+function home-first-bin() {
+  while read -r -d ':' item; do
+    if [[ "$item" == "$HOME/"* ]]; then
+      echo "$item";
+      return 0;
+    fi
+  done < <(echo "$PATH")
+  return -1;
+}
+
+function get-target() {
+  if [[ -n "$LOCAL_TARGET" ]]; then
+    echo "$LOCAL_TARGET";
+    return 0;
+  fi
+  local TARGET;
+  local CONF_FILE="$(conf-file)"
+  if [[ -e "$CONF_FILE" ]]; then
+    . "$CONF_FILE"
+  fi
+  if [[ -n "$TARGET" ]]; then
+    echo "$TARGET";
+    return 0;
+  fi
+  if [[ $UID == 0 ]]; then
+    echo "/usr/local/bin"
+    return 0;
+  fi
+  home-first-bin
+}
+
+function get-sources() {
+  if [[ -n "$LOCAL_SOURCES" ]]; then
+    echo "$LOCAL_SOURCES";
+    return 0;
+  fi
+  local SOURCES;
+  local CONF_FILE="$(conf-file)"
+  if [[ -e "$CONF_FILE" ]]; then
+    . "$CONF_FILE"
+  fi
+  if [[ -n "$SOURCES" ]]; then
+    echo "$SOURCES"
+    return 0;
+  fi
+  if [[ $UID == 0 ]]; then
+    echo "/opt"
+    return 0;
+  fi
+  echo "$HOME/.repos"
+  return 0;
+}
+
+function normalize-repo() {
+  if [[ "$1" == *"@"*":"*"/"* || "$1" == "https://"* || "$1" == *"/"*"/"* ]]; then
+    echo "$1";
+    return;
+  fi
+  if [[ "$1" == *"/"* ]]; then
+    echo "https://github.com/$1.git";
+    return;
+  fi
+  # check installed repos
+  REPO="$(get-sources)/$1"
+  if [[ -d "$REPO/.git" ]]; then
+    if [[ "$(stat -c '%U' "$REPO")" == "$USER" ]]; then
+      git remote get-url origin 2>/dev/null && return;
+    fi
+  fi
+
+  # assume...
+  echo "git@github.com:$USER/$1.git"
+}
+
+function project-name() {
+  basename -s .git "$1"
+}
+
+function dump-config() {
+  echo "Configuration file: $(conf-file)"
+  echo "Source folder: $(get-sources)"
+  echo "Executables: $(get-target)"
+}
+
+function set-value() {
+  local CONF_FILE="$(conf-file)"
+  local name="$1"; shift
+  local value="$1"; shift
+  if [[ ! -d "$CONF_PATH" ]]; then
+    mkdir -p "$(dirname "$CONF_PATH")";
+  fi
+  if [[ ! -f "$CONF_FILE" ]]; then
+    touch "$CONF_FILE";
+  fi
+  TMP="$(mktemp)"
+  cat "$CONF_FILE" | grep -Ev "^$name=" > "$TMP"
+  echo "$name=\"$value\"" >> "$TMP"
+  rm "$CONF_FILE"
+  mv "$TMP" "$CONF_FILE"
+}
+
+function list-sources() {
+  SOURCE="$(dirname "$(find "$(get-sources)" -wholename "*/.git")")"
+  if [[ -d "$SOURCE/bin" ]]; then
+    echo "$SOURCE"
+  fi
+}
+
+function repo-for-source() {
+  cd "$1"
+  git remote get-url origin
+}
+
+function list-links() {
+  local target="$(get-target)"
+  find "$target" -lname "$1/*"
+}
+
+function install-links() {
+  local source="$1"; shift
+  local target="$(get-target)"
+  while read bin; do
+    name="$(basename "$bin")"
+    ln -s "$source/bin/$name" "$target/$name"
+  done < <(find "$source/bin" -executable -type f)
+}
+
+function uninstall-links() {
+  local source="$1"; shift
+  local target="$(get-target)"
+  while read link; do
+    rm "$link"
+  done < <(list-links "$source")
+}
+
+function assert-args() {
+  if [[ "${#ARGS[@]}" != "$1" ]]; then
+    echo "Expected $1 positional arguments; got ${#ARGS[@]}."
+    exit -1
+  fi
+}
+
+function install-repo() {
+  REPO="$(normalize-repo "$1")"
+  SOURCE="$(get-sources)/$(project-name "$REPO")"
+  if [[ -z "$BRANCH" ]]; then
+    git clone --depth=1 "$REPO" "$SOURCE" >&2
+  else
+    git clone --depth=1 -b "$BRANCH" "$REPO" "$SOURCE" >&2
+  fi
+  install-links "$SOURCE"
+}
+
+function uninstall-repo() {
+  REPO="$(normalize-repo "${ARGS[0]}")"
+  SOURCE="$(get-sources)/$(project-name "$REPO")"
+  uninstall-links "$SOURCE"
+  rm -rf "$SOURCE"
+}
+
+function update-repo() {
+  local source="$1"; shift
+  if [[ -n "$BRANCH" ]]; then
+    local repo="$(repo-for-source "$source")"
+    uninstall-repo "$repo"
+    install-repo "$repo"
+  else
+    cd "$source"
+    git pull
+  fi
+}
+
+
+function update-self() {
+  SELF="$(get-target)/update-repo"
   TYPE="Installed"
   if [[ -f "$SELF" ]]; then
     TYPE="Updated"
   fi
   wget https://raw.githubusercontent.com/Fordi/update-repo/refs/heads/main/update-repo.sh -qO "$SELF-tmp"
   chmod +x "$SELF-tmp"
-  if [[ "$REPO" == "all" ]]; then
-    while read git; do
-      dir="$(dirname "$git")"
-      if [[ -d "$dir/bin" ]]; then
-        cd "$dir"
-        "$SELF-tmp" "$(git remote get-url origin)"
-      fi
-    done < <(find /opt -wholename '*/.git')
-  fi
-  if [[ -f "$SELF" ]]; then
+    if [[ -f "$SELF" ]]; then
     rm "$SELF"
   fi
   mv "$SELF-tmp" "$SELF"
   echo "${TYPE}"' `update-repo` command' >&2
-  exit
-fi
+}
 
-SOURCES="/opt/$(basename "$REPO")"
-if [[ -x "$SOURCES" ]]; then
-  cd "$SOURCES";
-  echo "Updating $(basename "$SOURCES")"
-  git pull
-else
-  git clone --depth 1 "$REPO" "$SOURCES" > /dev/null 2>&1
-fi
-cd "$SOURCES"
+ARGS=()
+SUBCOMMAND=usage
+ALL=0
+BRANCH=
+VERBOSE=0
+while ((${#})); do case "$1" in
+  U | update | UA)
+    SUBCOMMAND=update
+    if [[ "$1" == "UA" ]]; then
+      ALL=1
+    fi
+  ;;
+  d | dump-config)
+    SUBCOMMAND=dump-config
+  ;;
+  c | configure)
+    SUBCOMMAND=configure
+  ;;
+  i | install)
+    SUBCOMMAND=install
+  ;;
+  r | remove)
+    SUBCOMMAND=remove
+  ;;
+  ls | list)
+    SUBCOMMAND=list
+  ;;
+  -b | --branch)
+    BRANCH="$2"; shift
+  ;;
+  -s | --sources)
+    LOCAL_SOURCES="$2"; shift
+  ;;
+  -t | --target)
+    LOCAL_TARGET="$2"; shift
+  ;;
+  -c | --configuration)
+    LOCAL_CONF="$2"; shift
+  ;;
+  -v | --verbose)
+    VERBOSE=1;
+  ;;
+  *)
+    ARGS+=($1)
+  ;;
+esac; shift; done
 
-while read link; do
-  rm "$link"
-done < <(find "$TARGET" -type l -lname "$SOURCES/*")
 
-cd "$SOURCES/bin"
-while read file; do
-  name="$(basename "$file")";
-  echo "Symlinking $SOURCES/bin/$file to $TARGET/$name"
-  ln -s "$SOURCES/bin/$file" "$TARGET/$name"
-done < <(find . -type f -executable -not -path './.git/*')
+
+case "$SUBCOMMAND" in
+  dump-config)
+    assert-args 0
+    dump-config
+    exit
+  ;;
+  configure)
+    assert-args 0
+    set-value SOURCES "$(get-sources)"
+    set-value TARGET "$(get-target)"
+    exit
+  ;;
+  install)
+    assert-args 1
+    install-repo "${ARGS[0]}"
+  ;;
+  remove)
+    assert-args 1
+    uninstall-repo "${ARGS[0]}"
+  ;;
+  list)
+    assert-args 0
+    if [[ $VERBOSE == 0 ]]; then
+      list-sources
+    else
+      while read source; do
+        echo "$source"
+        echo "  repo: $(repo-for-source $source)"
+        echo "  binaries:"
+        while read link; do
+          echo "    $link"
+        done < <(list-links $source)
+      done < <(list-sources)
+    fi
+  ;;
+  update)
+    if [[ $ALL == 1 ]]; then
+      assert-args 0
+    else
+      assert-args 1
+    fi
+    if [[ "${ARGS[0]}" == "all" ]]; then
+      ALL=1
+    fi
+    if [[ "${ARGS[0]}" == "self" ]]; then
+      update-self
+      exit
+    fi
+    if [[ $ALL == 1 ]]; then
+      while read source; do
+        cd "$source"
+        git pull
+        uninstall-links "$source"
+        install-links "$source"
+      done < <(list-sources)
+    else
+      REPO="$(normalize-repo "${ARGS[0]}")"
+      SOURCE="$(get-sources)/$(project-name "$REPO")"
+      cd "$SOURCE"
+      git pull
+      uninstall-links "$source"
+      install-links "$source"
+    fi
+  ;;
+  usage)
+    if [[ "$SELF" == "bash" || "$0" == "/dev/stdin" ]]; then
+      update-self
+      exit
+    fi
+    echo "$SELF <subcommand> [...options]"
+    echo "    ls | list - list installed repositories"
+    echo "    u | uninstall [repo] - uninstall repository"
+    echo "    i | install [repo] - install a repository"
+    echo "      -b | -branch [branch] - specify a preferred branch"
+    echo "    c | configure - modify global configuration"
+    echo "    d | dump-config - dump the current global configuration"
+    echo "    U | update [repo] - update repository"
+    echo "      -b | -branch [branch] - specify a preferred branch"
+    echo "    UA | update all - update all installed repositories"
+    echo ""
+    echo "Flags:"
+    echo "    -s | -sources - specify sources folder (default /opt or ~/.repos)"
+    echo "    -t | -target - specify target folder (default /usr/local/bin or first item in PATH under ~)"
+    echo ""
+    echo "[repo] can be:"
+    echo "    [project-name] - a GitHub repo assumed to be owned by \$USER, e.g.,"
+    echo "      \`update-repo install foo\` for user $USER would be treated as git@github.com:$USER/foo.git"
+    echo "    [owner/project] - a GitHub repo; \`foo/bar\` would become https://github.com/foo/bar.git"
+    echo "    [url] - a full URL to a git repo; SSH and HTTPS supported"
+  ;;
+esac
 
