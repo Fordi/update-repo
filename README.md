f490eb84e0ab0579d9b869cb71d3e9f0112646cb
Bryan Elliott <fordiman@gmail.com>
2025-05-08 12:54:28 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# update-repo
+
+Usage:
+```bash
+wget https://fordi.github.io/update-repo/update-repo.sh -qO - | sudo bash /dev/stdin https://github.com/Fordi/sfv-tools
+```
+
+Target repository must have a `bin` folder containing executables to be installed to `/usr/local/bin`.
+
---
+++ update-repo.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+REPO="$1"; shift
+TARGET="${1:-/usr/local/bin}"; shift
+
+if [[ -z "$REPO" ]]; then
+  echo "$(basename "$0") [repository URL]" >&2
+  exit -1
+fi
+
+SOURCES="/opt/$(basename "$REPO")"
+if [[ -x "$SOURCES" ]]; then
+  cd "$SOURCES";
+  echo "Updating $(basename "$SOURCES")"
+  git pull
+else
+  git clone --depth 1 "$REPO" "$SOURCES" > /dev/null 2>&1
+fi
+cd "$SOURCES"
+HERE="$SOURCES"
+
+function install() {
+  local file="$1"; shift;
+  local name="$(basename "$1")";
+  if [[ -f "$TARGET/$name" ]]; then
+    rm "$TARGET/$name"
+  fi
+  echo "Symlinking $HERE/$file to $TARGET/$name"
+  ln -s "$HERE/$file" "$TARGET/$name"
+}
+
+cd bin
+while read file; do
+  install "$file"
+done < <(find . -type f -executable -not -path './.git/*')
+
